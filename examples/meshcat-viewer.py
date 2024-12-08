1b112ed66439ad188fd82470a0a9bf2a92d7c329
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-08 20:47:32 +0100

    Rebase from 'upstream'

--- examples/meshcat-viewer.py
+++ examples/meshcat-viewer.py
@@ -5,7 +5,6 @@
 import pinocchio as pin
 import numpy as np
 import sys
-import os
 from os.path import dirname, join, abspath
 
 from pinocchio.visualize import MeshcatVisualizer
@@ -25,8 +24,6 @@ model, collision_model, visual_model = pin.buildModelsFromUrdf(
     urdf_model_path, mesh_dir, pin.JointModelFreeFlyer()
 )
 
-viz = MeshcatVisualizer(model, collision_model, visual_model)
-
 # Start a new MeshCat server and client.
 # Note: the server can also be started separately using the "meshcat-server" command in a terminal:
 # this enables the server to remain active after the current script ends.
@@ -34,6 +31,7 @@ viz = MeshcatVisualizer(model, collision_model, visual_model)
 # Option open=True pens the visualizer.
 # Note: the visualizer can also be opened seperately by visiting the provided URL.
 try:
+    viz = MeshcatVisualizer(model, collision_model, visual_model)
     viz.initViewer(open=True)
 except ImportError as err:
     print(
@@ -48,19 +46,29 @@ viz.loadViewerModel()
 # Display a robot configuration.
 q0 = pin.neutral(model)
 viz.display(q0)
-viz.displayCollisions(True)
-viz.displayVisuals(False)
+viz.displayVisuals(True)
 
+# Create a convex shape from solo main body
 mesh = visual_model.geometryObjects[0].geometry
 mesh.buildConvexRepresentation(True)
 convex = mesh.convex
 
+# Place the convex object on the scene and display it
 if convex is not None:
     placement = pin.SE3.Identity()
     placement.translation[0] = 2.0
-    geometry = pin.GeometryObject("convex", 0, convex, placement)
+    geometry = pin.GeometryObject("convex", 0, placement, convex)
     geometry.meshColor = np.ones((4))
+    # Add a PhongMaterial to the convex object
+    geometry.overrideMaterial = True
+    geometry.meshMaterial = pin.GeometryPhongMaterial()
+    geometry.meshMaterial.meshEmissionColor = np.array([1.0, 0.1, 0.1, 1.0])
+    geometry.meshMaterial.meshSpecularColor = np.array([0.1, 1.0, 0.1, 1.0])
+    geometry.meshMaterial.meshShininess = 0.8
     visual_model.addGeometryObject(geometry)
+    # After modifying the visual_model we must rebuild
+    # associated data inside the visualizer
+    viz.rebuildData()
 
 # Display another robot.
 viz2 = MeshcatVisualizer(model, collision_model, visual_model)
