80fdc1b4f5afef94d5d84eb34f982b6e826d5a5a
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-20 12:16:07 +0100

    Rebase from 'release/humble/pinocchio'

--- include/pinocchio/parsers/mjcf/mjcf-graph.hpp
+++ include/pinocchio/parsers/mjcf/mjcf-graph.hpp
@@ -52,7 +52,7 @@ namespace pinocchio
         // Value for angle conversion (Mujoco default - degrees)
         double angle_converter = boost::math::constants::pi<double>() / 180.0;
         // Euler Axis to use to convert angles representation to quaternion
-        Eigen::Matrix3d mapEulerAngles;
+        Eigen::Matrix3d mapEulerAngles = Eigen::Matrix3d::Identity();
 
         // Value to crop the mass (if mass < boundMass, mass = boundMass)
         double boundMass = 0;
@@ -135,7 +135,7 @@ namespace pinocchio
         Eigen::VectorXd damping;
 
         // Armature inertia created by this joint
-        double armature = 0.;
+        Eigen::VectorXd armature;
         // Dry friction.
         double frictionLoss = 0.;
 
@@ -149,9 +149,9 @@ namespace pinocchio
           maxConfig = Eigen::VectorXd::Constant(1, infty);
           springStiffness = Eigen::VectorXd::Constant(1, v);
           springReference = Eigen::VectorXd::Constant(1, v);
-          ;
           friction = Eigen::VectorXd::Constant(1, 0.);
           damping = Eigen::VectorXd::Constant(1, 0.);
+          armature = Eigen::VectorXd::Constant(1, 0.);
         }
 
         /// @brief Set dimension to the limits to match the joint nq and nv.
@@ -209,6 +209,8 @@ namespace pinocchio
         Eigen::Vector3d scale = Eigen::Vector3d::Constant(1);
         // Path to the mesh file
         std::string filePath;
+        // Vertices of the mesh
+        Eigen::MatrixX3d vertices;
       };
 
       /// @brief All informations related to a texture are stored here
@@ -446,7 +448,7 @@ namespace pinocchio
         /// @brief Go through the default part of the file and get all the class name. Fill the
         /// mapOfDefault for later use.
         /// @param el ptree element. Root of the default
-        void parseDefault(ptree & el, const ptree & parent);
+        void parseDefault(ptree & el, const ptree & parent, const std::string & parentTag);
 
         /// @brief Go through the main body of the mjcf file "worldbody" to get all the info ready
         /// to create the model.
@@ -547,7 +549,7 @@ namespace pinocchio
         inline std::istringstream getConfiguredStringStream(const std::string & str)
         {
           std::istringstream posStream(str);
-          posStream.exceptions(std::ios::failbit);
+          posStream.exceptions(std::ios::badbit);
           return posStream;
         }
 
@@ -567,9 +569,8 @@ namespace pinocchio
           std::istringstream stream = getConfiguredStringStream(str);
           std::vector<double> vector;
           double elem;
-          while (!stream.eof())
+          while (stream >> elem)
           {
-            stream >> elem;
             vector.push_back(elem);
           }
 
