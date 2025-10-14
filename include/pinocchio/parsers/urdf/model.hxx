bdeb76dd73b89a450d37aac070a0f8886e218e54
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-14 18:55:21 +0200

    Rebase from 'debian/jazzy/pinocchio'

--- include/pinocchio/parsers/urdf/model.hxx
+++ include/pinocchio/parsers/urdf/model.hxx
@@ -314,6 +314,15 @@ namespace pinocchio
                   frame, placement, joint_name, max_effort, max_velocity, min_config, max_config,
                   friction, damping, *mimic_info);
                 break;
+              case Base::CONTINUOUS:
+                joint_id = addMimicJoint<
+                  typename JointCollection::JointModelRUBX,
+                  typename JointCollection::JointModelRUBY,
+                  typename JointCollection::JointModelRUBZ,
+                  typename JointCollection::JointModelRevoluteUnboundedUnaligned>(
+                  frame, placement, joint_name, max_effort, max_velocity, min_config, max_config,
+                  friction, damping, *mimic_info);
+                break;
               default:
                 PINOCCHIO_CHECK_INPUT_ARGUMENT(
                   false, "Cannot mimic this type. Only revolute, prismatic and helicoidal can be "
@@ -531,7 +540,8 @@ namespace pinocchio
             return model.addJoint(
               frame.parentJoint,
               typename JointCollection::JointModelMimic(
-                TypeUnaligned(), mimicked_joint, mimic_info.multiplier, mimic_info.offset),
+                TypeUnaligned(mimic_info.axis.normalized()), mimicked_joint, mimic_info.multiplier,
+                mimic_info.offset),
               frame.placement * placement, joint_name, max_effort, max_velocity, min_config,
               max_config, friction, damping);
             break;
